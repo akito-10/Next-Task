490495df88430daa70e503d7e98b23a4b38faf28
github akito-10 <akt.fukuda@gmail.com>
2021-04-21 23:22:49 +0900

    ユーザーアイコンを表示

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,15 +1,21 @@
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectUser } from "src/features/userSlice";
 import { Button } from "./shared/Button";
 
 export const MainContent = (): JSX.Element => {
+  const user = useSelector(selectUser);
+
   return (
     <div className="text-center text-gray-50">
-      <Image
-        src="/profile.png"
-        width={100}
-        height={100}
-        className="rounded-full"
-      />
+      {user.photoUrl && (
+        <img
+          src={user.photoUrl}
+          width={100}
+          height={100}
+          className="rounded-full mx-auto"
+        />
+      )}
       <h2 className="text-3xl mt-4">ユーザーネーム</h2>
       <h3 className="text-xl mt-12">進行中のタスク</h3>
       <div className="bg-gray-50 text-gray-500 px-16 py-4 mt-4 rounded-lg shadow-xl">
