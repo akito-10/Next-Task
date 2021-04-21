1c2043542d763e9414be4162aa81727b67ee68b1
github akito-10 <akt.fukuda@gmail.com>
2021-04-21 22:01:29 +0900

    redux用の記述を_app.tsxに追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "ress";
 import { AppProps } from "next/app";
 import "src/styles/global.css";
+import { Provider } from "react-redux";
+import { store } from "src/app/store";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  useEffect(() => {
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement?.removeChild(jssStyles);
-    }
-  }, []);
-
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
