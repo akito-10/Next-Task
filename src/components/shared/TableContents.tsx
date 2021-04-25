5ed80d22578cd4ef5e2c122ef15350f96364c372
github akito-10 <akt.fukuda@gmail.com>
2021-04-25 19:07:59 +0900

    ゴミ箱アイコンにポインタのスタイル適応

--- src/components/shared/TableContents.tsx
+++ src/components/shared/TableContents.tsx
@@ -21,7 +21,7 @@ export const TableContents = ({
       <td className="px-6 py-4 w-1/5">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-full mx-auto"
+          className="h-6 w-full mx-auto cursor-pointer"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
