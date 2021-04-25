cae364e74ec94845de71d5fc669e924265560ab8
github akito-10 <akt.fukuda@gmail.com>
2021-04-25 17:27:58 +0900

    インプットコンポーネントにprops追加

--- src/components/shared/InputField.tsx
+++ src/components/shared/InputField.tsx
@@ -1,15 +1,18 @@
-import React, { ChangeEvent, Dispatch, SetStateAction } from "react";
+import classNames from "classnames";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
 type InputFieldProps = {
+  color?: "white" | "gray";
   name: string;
-  type: "text" | "email" | "password";
-  autoComplete: string;
-  placeholder: string;
+  type: "text" | "email" | "password" | "date";
+  autoComplete?: string;
+  placeholder?: string;
   value: string;
   onChange: Dispatch<SetStateAction<string>>;
 };
 
 export const InputField = ({
+  color = "gray",
   name,
   type,
   autoComplete,
@@ -17,6 +20,8 @@ export const InputField = ({
   value,
   onChange,
 }: InputFieldProps): JSX.Element => {
+  const bgColor = color === "gray" ? "bg-gray-300" : "bg-white";
+
   return (
     <>
       <input
@@ -24,12 +29,17 @@ export const InputField = ({
         type={type}
         autoComplete={autoComplete}
         required
-        className="appearance-none rounded-none relative block w-full px-3 py-2 mb-3 border border-gray-300 placeholder-gray-500 text-gray-600 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+        className={classNames(
+          "appearance-none relative block w-full px-3 py-2 mb-3 border border-gray-300 placeholder-gray-500 text-gray-600 rounded-lg focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm",
+          bgColor
+        )}
         placeholder={placeholder}
         value={value}
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          onChange(e.target.value)
-        }
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          onChange(e.target.value);
+          console.log(e.target.value);
+          console.log(typeof e.target.value);
+        }}
       />
     </>
   );
