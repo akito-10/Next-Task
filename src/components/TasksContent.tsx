2ce9768386080dda17babcf0cf3d3c19753ef59b
github akito-10 <akt.fukuda@gmail.com>
2021-04-26 23:00:25 +0900

    ページネーションを追加

--- src/components/TasksContent.tsx
+++ src/components/TasksContent.tsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { TableContents } from "src/components/shared/TableContents";
 import { PrimaryButton } from "./shared/PrimaryButton";
 
@@ -23,16 +25,56 @@ const TABLE_CONTENTS_VALUE = [
     name: "個人開発アプリ４",
     progress: "25%",
   },
+  // {
+  //   id: "5",
+  //   name: "個人開発アプリ５",
+  //   progress: "25%",
+  // },
 ];
+
+type Table_Contents_Type = {
+  title: string;
+  created_at: any;
+  todoList: {
+    title: string;
+    deadline: string;
+    isDone: boolean | null;
+    doneDate: any;
+  }[];
+}[];
 
 export const TasksContent = (): JSX.Element => {
   const router = useRouter();
+  const [tableContents, setTableContents] = useState<Table_Contents_Type>([
+    {
+      title: "",
+      created_at: null,
+      todoList: [
+        {
+          title: "",
+          deadline: "",
+          isDone: null,
+          doneDate: null,
+        },
+      ],
+    },
+  ]);
+
+  const classes =
+    tableContents.length > 3 || TABLE_CONTENTS_VALUE.length > 3
+      ? "h-auto"
+      : "h-80";
 
   return (
     <div className="flex flex-col max-w-full -my-8 sm:my-0">
       <div className="-my-2">
-        <div className="py-2 align-middle">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg  w-650px max-w-full mx-auto">
+        <div className="py-2 align-middle bg-gray-50">
+          <div
+            className={classNames(
+              "shadow overflow-hidden border-b border-gray-200 sm:rounded-t-lg w-650px max-w-full mx-auto",
+              classes
+            )}
+          >
             <table className="divide-y divide-gray-200 w-full">
               <thead className="bg-gray-50">
                 <tr>
@@ -60,12 +102,118 @@ export const TasksContent = (): JSX.Element => {
                 {TABLE_CONTENTS_VALUE.map((content) => (
                   <TableContents
                     key={content.id}
+                    className="h-14"
                     name={content.name}
                     sub={content.progress}
+                    onClick={() => console.log("OK!!")}
                   />
                 ))}
               </tbody>
             </table>
+          </div>
+          <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+            <div className="flex-1 flex justify-between sm:hidden">
+              <a
+                href="#"
+                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
+              >
+                Previous
+              </a>
+              <a
+                href="#"
+                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
+              >
+                Next
+              </a>
+            </div>
+            <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-end">
+              <div>
+                <nav
+                  className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+                  aria-label="Pagination"
+                >
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                  >
+                    <span className="sr-only">Previous</span>
+                    {/* <!-- Heroicon name: solid/chevron-left --> */}
+                    <svg
+                      className="h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    1
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    2
+                  </a>
+                  <a
+                    href="#"
+                    className="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    3
+                  </a>
+                  <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
+                    ...
+                  </span>
+                  <a
+                    href="#"
+                    className="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    8
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    9
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    10
+                  </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                  >
+                    <span className="sr-only">Next</span>
+                    {/* <!-- Heroicon name: solid/chevron-right --> */}
+                    <svg
+                      className="h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </nav>
+              </div>
+            </div>
           </div>
         </div>
         <PrimaryButton
