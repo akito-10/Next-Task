74df599ca92db15a041b3bc10ad97d161deb4d1a
github akito-10 <akt.fukuda@gmail.com>
2021-05-05 13:05:47 +0900

    タスク一覧のボタンのレイアウト変更

--- src/components/TasksContent.tsx
+++ src/components/TasksContent.tsx
@@ -139,18 +139,12 @@ export const TasksContent = (): JSX.Element => {
           </div>
           <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
             <div className="px-6 flex-1 flex justify-between">
-              <a
-                href="#"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-              >
+              <p className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 cursor-pointer">
                 ＜Previous
-              </a>
-              <a
-                href="#"
-                className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-              >
+              </p>
+              <p className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 cursor-pointer">
                 Next＞
-              </a>
+              </p>
             </div>
           </div>
         </div>
